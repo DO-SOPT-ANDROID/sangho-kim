fe4c2dc1fc043cb2312d387784a0752b74daf534
Sangho Kim <marchbreeze03@gmail.com>
2023-10-09 20:47:48 +0900

    [ADD/#1] Binding & Util 함수 추가

---
+++ app/src/main/java/org/sopt/dosopttemplate/util/view/ViewExt.kt
@@ -0,0 +1,40 @@
+package org.sopt.dosopttemplate.util.view
+
+import android.content.Context
+import android.util.TypedValue
+import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+
+inline fun View.setOnSingleClickListener(
+    delay: Long = 1000L,
+    crossinline block: (View) -> Unit,
+) {
+    var previousClickedTime = 0L
+    setOnClickListener { view ->
+        val clickedTime = System.currentTimeMillis()
+        if (clickedTime - previousClickedTime >= delay) {
+            block(view)
+            previousClickedTime = clickedTime
+        }
+    }
+}
+
+class ItemDiffCallback<T : Any>(
+    val onItemsTheSame: (T, T) -> Boolean,
+    val onContentsTheSame: (T, T) -> Boolean,
+) : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(
+        oldItem: T,
+        newItem: T,
+    ): Boolean = onItemsTheSame(oldItem, newItem)
+
+    override fun areContentsTheSame(
+        oldItem: T,
+        newItem: T,
+    ): Boolean = onContentsTheSame(oldItem, newItem)
+}
+
+fun Int.dpToPx(context: Context): Int {
+    val metrics = context.resources.displayMetrics
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), metrics).toInt()
+}
