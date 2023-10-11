a7c86558f127f9c5983c85d190603936a996fe39
Sangho Kim <marchbreeze03@gmail.com>
2023-10-11 15:05:13 +0900

    [FIX/#1] 의진님 코드리뷰 반영

--- app/src/main/java/org/sopt/dosopttemplate/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/MainActivity.kt
@@ -1,5 +1,8 @@
 package org.sopt.dosopttemplate.presentation
 
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.model.User
@@ -7,6 +10,7 @@ import org.sopt.dosopttemplate.databinding.ActivityMainBinding
 import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.EXTRA_USER
 import org.sopt.dosopttemplate.util.base.BindingActivity
 import org.sopt.dosopttemplate.util.intent.getParcelable
+import toast
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
@@ -24,8 +28,21 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     private fun setUiText() {
-        binding.tvMainNickname.text = userData.nickname
-        binding.tvMainId.text = userData.id
-        binding.tvMainDrink.text = userData.drink
+        if (::userData.isInitialized) {
+            binding.tvMainNickname.text = userData.nickname
+            binding.tvMainId.text = userData.id
+            binding.tvMainDrink.text = userData.drink
+        } else {
+            toast("다시 로그인해주세요.")
+            returnToLoginActivity()
+        }
+    }
+
+    private fun returnToLoginActivity() {
+        Intent(this, LoginActivity::class.java).apply {
+            addFlags(FLAG_ACTIVITY_CLEAR_TASK or FLAG_ACTIVITY_NEW_TASK)
+            startActivity(this)
+        }
+        finish()
     }
 }
