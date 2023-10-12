b62b4215925d5c5d747574d6ae2be9dfe5317acd
Sangho Kim <marchbreeze03@gmail.com>
2023-10-13 01:02:22 +0900

    [FEAT/#3] AuthSharedPref 구현

--- app/src/main/java/org/sopt/dosopttemplate/data/datasource/local/AuthSharedPref.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/datasource/local/AuthSharedPref.kt
@@ -1,4 +1,38 @@
 package org.sopt.dosopttemplate.data.datasource.local
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.sopt.dosopttemplate.data.model.User
+
 class AuthSharedPref {
+    private lateinit var authPref: SharedPreferences
+    private val userGson: Gson = GsonBuilder().create()
+
+    fun initAuthPref(context: Context) {
+        authPref = context.getSharedPreferences(
+            PREF_AUTH_TITLE,
+            Context.MODE_PRIVATE,
+        )
+    }
+
+    fun setAuthUser(user: User) {
+        authPref.edit().putString(PREF_AUTH_USER, userGson.toJson(user)).apply()
+    }
+
+    fun getAuthUser() : User? {
+        val user = authPref.getString(PREF_AUTH_USER, null)
+        if (user.isNullOrBlank()) return null
+        return userGson.fromJson(user, User::class.java)
+    }
+
+    fun clearAuthPref() {
+        authPref.edit().clear().apply()
+    }
+
+    companion object {
+        private const val PREF_AUTH_TITLE = "AUTH_PREF"
+        private const val PREF_AUTH_USER = "AUTH_USER"
+    }
 }
