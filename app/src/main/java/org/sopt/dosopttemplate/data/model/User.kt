4255bf042186025cd3d391e9747c84ff327e368f
Sangho Kim <marchbreeze03@gmail.com>
2023-10-22 20:56:08 +0900

    [ADD/#7] Friends data 설정

--- app/src/main/java/org/sopt/dosopttemplate/data/model/User.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/User.kt
@@ -2,6 +2,7 @@ package org.sopt.dosopttemplate.data.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import org.sopt.dosopttemplate.R
 
 @Parcelize
 data class User(
@@ -9,15 +10,22 @@ data class User(
     val pw: String,
     val nickname: String = "",
     val drink: String = "",
+    val drinkAmount: String? = "",
+    val thumbnail: Int? = R.drawable.ic_launcher_background,
     val description: String? = "",
-    val musicArtist: String? = "",
-    val musicTitle: String? = "",
     val isBirthday: Boolean = false
 ) : Parcelable
 
-fun emptyUser(): User {
-    return User(id = "", pw = "")
-}
+fun emptyUser() = User(id = "", pw = "")
+
+fun friendUser(
+    nickname: String,
+    drink: String = "",
+    drinkAmount: String? = "",
+    description: String? = "",
+    thumbnail: Int? = R.drawable.ic_launcher_background,
+    isBirthday: Boolean = false
+) = User("", "", nickname, drink, drinkAmount, thumbnail, description, isBirthday)
 
 fun isUserEmpty(user: User) =
     listOf(user.id, user.pw, user.nickname, user.drink).any { it.isEmpty() }
