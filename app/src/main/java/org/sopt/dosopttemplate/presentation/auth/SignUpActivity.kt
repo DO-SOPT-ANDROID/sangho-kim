5ee5493674cba61bb6082ecc47b4fe36da0cae8b
Sangho Kim <marchbreeze03@gmail.com>
2023-12-04 18:14:18 +0900

    [ADD/#16] 뷰모델 설정

--- app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignUpActivity.kt
@@ -2,17 +2,12 @@ package org.sopt.dosopttemplate.presentation.auth
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.databinding.ActivitySignUpBinding
 import org.sopt.dosopttemplate.domain.entity.User
 import org.sopt.dosopttemplate.presentation.auth.LoginActivity.Companion.EXTRA_USER
 import org.sopt.dosopttemplate.util.base.BindingActivity
 import org.sopt.dosopttemplate.util.extension.setOnSingleClickListener
-import org.sopt.dosopttemplate.util.extension.snackBar
 import org.sopt.dosopttemplate.util.extension.toast
 
 class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
@@ -23,36 +18,13 @@ class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_
         super.onCreate(savedInstanceState)
 
         initSignUpBtnListener()
-        observeSignUpAvailable()
         observeSignUpResult()
     }
 
     private fun initSignUpBtnListener() {
         binding.btnSignUp.setOnSingleClickListener {
-            viewModel.setEditedUser(with(binding) {
-                User(
-                    etSignUpId.text.toString().trim(),
-                    etSignUpPw.text.toString().trim(),
-                    etSignUpNickname.text.toString().trim(),
-                    etSignUpDrink.text.toString().trim(),
-                )
-            })
-            viewModel.checkSignUpAvailable()
+            viewModel.postSignUpToServer()
         }
-    }
-
-    private fun observeSignUpAvailable() {
-        viewModel.checkSignUpState.flowWithLifecycle(lifecycle).onEach { state ->
-            when (state) {
-                is AuthState.IdError -> snackBar(binding.root) { getString(R.string.sign_in_id_error) }
-
-                is AuthState.PwError -> snackBar(binding.root) { getString(R.string.sign_in_pw_error) }
-
-                is AuthState.EmptyError -> snackBar(binding.root) { getString(R.string.sign_in_empty_error) }
-
-                is AuthState.Success -> viewModel.postSignUpToServer()
-            }
-        }.launchIn(lifecycleScope)
     }
 
     private fun observeSignUpResult() {
--- app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignUpViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignUpViewModel.kt
@@ -3,25 +3,18 @@ package org.sopt.dosopttemplate.presentation.auth
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.launch
+import androidx.lifecycle.map
 import org.sopt.dosopttemplate.data.model.request.SignUpRequestDto
 import org.sopt.dosopttemplate.di.ServicePool
 import org.sopt.dosopttemplate.domain.entity.User
-import org.sopt.dosopttemplate.domain.entity.emptyUser
-import org.sopt.dosopttemplate.util.extension.checkLength
 import retrofit2.Call
 import retrofit2.Response
+import java.util.regex.Pattern
 
 class SignUpViewModel : ViewModel() {
 
-    private val _checkSignUpState = MutableSharedFlow<AuthState>()
-    val checkSignUpState: SharedFlow<AuthState>
-        get() = _checkSignUpState
-
-    private val _signUpState: MutableLiveData<ServerState<User>> = MutableLiveData(ServerState.Empty)
+    private val _signUpState: MutableLiveData<ServerState<User>> =
+        MutableLiveData(ServerState.Empty)
     val signUpState: LiveData<ServerState<User>> = _signUpState
 
     private val _idText: MutableLiveData<String> = MutableLiveData("")
@@ -33,33 +26,22 @@ class SignUpViewModel : ViewModel() {
     private val _drinkText: MutableLiveData<String> = MutableLiveData("")
     val drinkText: LiveData<String> = _drinkText
 
-    private var user = emptyUser()
+    private val isIdValid: LiveData<Boolean> = _idText.map { ID_REGEX.matcher(it).matches() }
+    private val isPwValid: LiveData<Boolean> = _pwText.map { PW_REGEX.matcher(it).matches() }
 
-    fun setEditedUser(editedUser: User?) {
-        user = editedUser ?: return
-    }
+    val isButtonValid: MutableLiveData<Boolean> = MutableLiveData(false)
 
-    fun checkSignUpAvailable() {
-        viewModelScope.launch {
-            val signUpResult = when {
-                !user.id.checkLength(6, 10) -> AuthState.IdError
-
-                !user.pw.checkLength(8, 12) -> AuthState.PwError
-
-                listOf(user.nickname, user.drink).any { it.isBlank() } -> AuthState.EmptyError
-
-                else -> AuthState.Success
-            }
-            _checkSignUpState.emit(signUpResult)
-        }
+    fun checkButtonValid() {
+        isButtonValid.value =
+            (isIdValid.value == true && isPwValid.value == true && !idText.value.isNullOrBlank() && !pwText.value.isNullOrBlank() && !nameText.value.isNullOrBlank() && !drinkText.value.isNullOrBlank())
     }
 
     fun postSignUpToServer() {
         ServicePool.authService.postSignUp(
             SignUpRequestDto(
-                username = user.id,
-                password = user.pw,
-                nickname = user.nickname
+                username = idText.value.orEmpty(),
+                password = pwText.value.orEmpty(),
+                nickname = nameText.value.orEmpty()
             )
         )
             .enqueue(object : retrofit2.Callback<Unit> {
@@ -68,7 +50,14 @@ class SignUpViewModel : ViewModel() {
                     response: Response<Unit>,
                 ) {
                     if (response.isSuccessful) {
-                        _signUpState.value = ServerState.Success(user)
+                        _signUpState.value = ServerState.Success(
+                            User(
+                                id = idText.value.orEmpty(),
+                                pw = pwText.value.orEmpty(),
+                                nickname = nameText.value.orEmpty(),
+                                drink = drinkText.value.orEmpty()
+                            )
+                        )
                     } else {
                         _signUpState.value = ServerState.Failure
                     }
@@ -78,5 +67,12 @@ class SignUpViewModel : ViewModel() {
                     _signUpState.value = ServerState.ServerError
                 }
             })
+    }
+
+    private companion object {
+        const val ID_PATTERN = """^(?=.*[a-zA-Z])(?=.*\d).{6,10}$"""
+        val ID_REGEX: Pattern = Pattern.compile(ID_PATTERN)
+        const val PW_PATTERN = """^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*()?]).{6,12}$"""
+        val PW_REGEX: Pattern = Pattern.compile(PW_PATTERN)
     }
 }
